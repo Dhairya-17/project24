954d2c04b46e44873cb54ac8c7921e2f1640e91c
Dhairya-17 <80893400+Dhairya-17@users.noreply.github.com>
2021-04-21 14:28:27 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var hammer,iron,stone,rubber;
+
+
+function setup(){
+    var canvas = createCanvas(1200,600);
+    engine = Engine.create();
+    world = engine.world;
+
+    plane = new Plane(600,height,1200,20)
+    hammer = new Hammer(10,100);
+rubber =  new Rubber(420,100,75)
+    stone = new Stone(600,100)
+    iron = new Iron(900,100)
+
+
+}
+
+function draw(){
+    background("lightBlue");
+    Engine.update(engine);
+
+
+    plane.display();
+    hammer.display();
+    rubber.display();
+    stone.display();
+    iron.display();
+}
